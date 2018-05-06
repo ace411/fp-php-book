3202e8b7391d6e2d80b1331dea9513fd41173543
ace411 <lochbm@live.com>
2018-05-06 20:59:46 +0300

    Added chapter one files

---
+++ chap-01/pure-add-function.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * add function
+ * 
+ * @package fp-in-php-primer
+ * @author Lochemem Bruno Michael
+ */
+
+function add(int $x, int $y) : int
+{
+    return $x + $y;
+}
+
+echo add(1, 2);
