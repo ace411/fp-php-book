319ca09c520b034d164c0e10aaa5f194b528953d
ace411 <lochbm@live.com>
2018-05-27 06:59:48 +0300

    Created first 5 chapter 5 files

---
+++ chap-05/rabbitmq-worker.php
@@ -0,0 +1,39 @@
+<?php
+
+require 'fibonacci.php';
+require __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use Chemem\Bingo\Functional\Algorithms as A;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+printf('%s', 'Waiting for input' . PHP_EOL);
+
+$callback = function ($msg) {
+    $result = A\compose(
+        A\partialRight('json_decode', true),
+        function (array $inputs) {
+            $result = fibGenerate(...$inputs);
+
+            return $result;
+        },
+        A\partialRight('json_encode', JSON_PRETTY_PRINT)
+    );
+
+    printf('%s', 'Processed ' . $result($msg));
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
