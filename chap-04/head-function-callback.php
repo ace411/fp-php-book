7a6eafac72e2bdc599f2a7b1fd200232649be91a
ace411 <lochbm@live.com>
2018-06-04 00:28:29 +0300

    Created first 3 chapter 4 files

---
+++ chap-04/head-function-callback.php
@@ -0,0 +1,8 @@
+<?php
+
+function head(array $numbers, callable $callback)
+{
+    return isset($numbers[0]) ? $numbers[0] : $callback($numbers);
+}
+
+echo head(['foo' => 12, 'bar' => 15], 'reset');
