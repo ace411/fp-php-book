a15ef65fa2ba0ba1ad68147a730f54527940fd24
ace411 <lochbm@live.com>
2018-09-13 11:54:07 +0300

    Modified pattern matching script

--- chap-07/pattern-matching-example.php
+++ chap-07/pattern-matching-example.php
@@ -2,12 +2,52 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use function FunctionalPHP\PatternMatching\match;
+use Chemem\Bingo\Functional\Algorithms as A;
+use Chemem\Bingo\Functional\Functors\Monads\Reader;
 
-$match = match(
+const POSTS = [
     [
-        '[_, "chemem"]' => function () { return 'chemem'; },
-        '_' => function () { return 'NaN'; }
+        'id' => 1,
+        'title' => 'Functional Programming Rocks',
+        'text' => 'fp is amazing.'
     ],
-    explode('/', 'api/chemem')
-);
+    [
+        'id' => 2,
+        'title' => 'Cannot wait for Star Wars Ep9',
+        'text' => 'Kylo Ren vs Rey part III.'
+    ]
+];
+
+const ERROR = [
+    'code' => 404,
+    'error' => 'Resource Not Found'
+];
+
+const matchFn = 'FunctionalPHP\\PatternMatching\\match';
+
+function resolvePath(string $path) : string
+{
+    $result = A\compose(
+        A\partialLeft('explode', '/'),
+        A\partialLeft(
+            matchFn,
+            [
+                '["post", id]' => function (string $id) {
+                    $res = A\compose(
+                        A\partialLeft(A\filter, function (array $post) use ($id) { return A\pluck($post, 'id') == (int) $id; }),
+                        A\head
+                    );
+
+                    return $res(POSTS);
+                },
+                '["posts"]' => function () { return A\identity(POSTS); },
+                '_' => function () { return A\identity(ERROR); }
+            ] 
+        ),
+        'json_encode'
+    );
+
+    return $result($path);
+}
+
+echo resolvePath('ringer');
