dac4381303153b9eb10e74dc9fe23efb6358e262
ace411 <lochbm@live.com>
2018-09-25 19:58:54 +0300

    Modified rabbitmq worker script

--- chap-05/rabbitmq-worker.php
+++ chap-05/rabbitmq-worker.php
@@ -3,9 +3,12 @@
 require 'fibonacci.php';
 require __DIR__ . '/vendor/autoload.php';
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
+use Chemem\Bingo\Functional\Functors\Monads\IO;
 use function Chemem\Bingo\Functional\Algorithms\{compose, partialRight};
-use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\{
+    Message\AMQPMessage,
+    Connection\AMQPStreamConnection
+};
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 
@@ -15,7 +18,7 @@ $channel->queue_declare('hello', false, false, false, false);
 
 printf('%s', 'Waiting for input' . PHP_EOL);
 
-$callback = function ($msg) {
+$callback = function ($msg) : IO {
     $result = compose(
         partialRight('json_decode', true),
         function (array $inputs) {
@@ -23,10 +26,13 @@ $callback = function ($msg) {
 
             return $result;
         },
-        partialRight('json_encode', JSON_PRETTY_PRINT)
+        'json_encode'
     );
 
-    printf('%s', 'Processed ' . $result($msg));
+    return IO::of($msg)
+        ->map(function ($msg) use ($result) {
+            return printf('%s', 'Processed ' . $result($msg->body) . \PHP_EOL);
+        });
 };
 
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
